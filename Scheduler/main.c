e1dc42799d1681933b7834cbc01144d325a88807
Christopher Linton <cjlinton83@gmail.com>
2019-04-03 19:07:51 -0500

    ..

--- Scheduler/main.c
+++ Scheduler/main.c
@@ -11,10 +11,14 @@ struct process {
     int arrival;
     int burst;
     int priority;
+    int first_in_queue;
+    int complete;
 };
 
 int load_process_buffer(struct process processes[], int n);
 void show_process_buffer(struct process processes[], int n);
+void fcfs(struct process processes[], int n);
+void show_process_stats(struct process processes[], int n, char *job_label);
 
 int main(void)
 {
@@ -22,13 +26,19 @@ int main(void)
     int process_count;
 
     if ((process_count = load_process_buffer(process_buffer, BUFFER_CAP)) != 0) {
-        printf("PROCESS COUNT: %d\n", process_count);
         show_process_buffer(process_buffer, process_count);
+        fcfs(process_buffer, process_count);
+        show_process_stats(process_buffer, process_count, "First Come, First Serve");
     }
 
     return 0;
 }
 
+/*
+ * load_process_buffer: loads process information, from a desctiption file,
+ *   into the processes buffer. The function loads up to n processes.
+ *   the function returns the number of processes loaded into the buffer.
+ */
 int load_process_buffer(struct process processes[], int n)
 {
     FILE *fp;
@@ -70,4 +80,50 @@ void show_process_buffer(struct process processes[], int n)
             processes[i].arrival,
             processes[i].burst,
             processes[i].priority);
+    printf("\n");
+}
+
+void fcfs(struct process processes[], int n)
+{
+    int in = processes[0].arrival;
+    int done = processes[0].burst;
+
+    processes[0].first_in_queue = in;
+    processes[0].complete = done;
+
+    for (int i = 1; i < n; i++) {
+        in = done;
+        done += processes[i].burst;
+        
+        processes[i].first_in_queue = in;
+        processes[i].complete = done;
+    }
+
+}
+
+void show_process_stats(struct process processes[], int n, char *job_label)
+{
+    int total_burst_time = 0;
+    int total_turn_around = 0;
+    int total_response = 0;
+
+    printf("Number of jobs in buffer: %d\n", n);
+    printf("Terminated Jobs (%s).\n", job_label);
+    printf("PID\tARRIVAL\tCOMPLETION\n");
+    printf("---\t-------\t----------\n");
+    
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t%d\n",
+            processes[i].id,
+            processes[i].arrival,
+            processes[i].complete);
+        total_burst_time += processes[i].burst;
+        total_turn_around += (processes[i].complete - processes[i].arrival);
+        total_response += (processes[i].first_in_queue - processes[i].arrival);
+    }
+    
+    printf("\nRun Stats:\n");
+    printf("Throughtput = %.2f\n", (float)n / (float)total_burst_time);
+    printf("Average turnaround time = %.2f\n", (float)total_turn_around / (float) n);
+    printf("Average response time = %.2f\n\n", (float)total_response / (float)n);
 }
